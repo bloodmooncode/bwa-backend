e62eb876823c5e7fe55c1dccb2fabf8888acafb0
ZYP <2359598489@qq.com>
2024-02-23 17:40:22 +0800

    Add Cors config class for 5277 port

--- src/main/java/blazor/Controller/ELementController.java
+++ src/main/java/blazor/Controller/ELementController.java
@@ -6,38 +6,47 @@ package blazor.Controller;
 
 import blazor.Entity.Element;
 import blazor.Service.ELementService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@CrossOrigin(origins = "http://localhost:5277")
+@Tag(name = "Elements")
 @RestController
 @RequestMapping("/microgrids")
 public class ELementController {
     @Autowired
     ELementService eLementService;
 
+    @Operation(summary = "Get Elements List of gridId")
     @GetMapping("/{microgridId}/elements/all")
     public List<Element> getAllElementsinOne(@PathVariable("microgridId") Integer microgridId) {
         return eLementService.getAllElementsinOne(microgridId);
     }
 
+    @Operation(summary = "Get one Element of gridId")
     @GetMapping("/{microgridId}/elements/{elementId}")
     public ResponseEntity<Element> getElementById(@PathVariable("microgridId") Integer microgridId, @PathVariable("elementId") Integer elementId) {
         return eLementService.getElementById(microgridId, elementId);
     }
 
+    @Operation(summary = "Create one Element in gridId")
     @PostMapping("/{microgridId}/elements/create")
     public void createELement(@PathVariable("microgridId") Integer microgridId, @RequestParam String name, String type, Double maximumBuyingPower, Double minimumBuyingPower, Double maximumSellingPower, Double minimumSellingPower) {
         eLementService.createELement(microgridId , name,  type, maximumBuyingPower, minimumBuyingPower, maximumSellingPower, minimumSellingPower);
     }
 
+    @Operation(summary = "Delete one Element in gridId")
     @DeleteMapping("/{microgridId}/elements/delete/{elementId}")
     public void deleteMicrogrid(@PathVariable("microgridId") Integer microgridId, @PathVariable("elementId") Integer elementId) {
         eLementService.deleteMicrogrid(microgridId, elementId);
     }
 
+    @Operation(summary = "Update one Element in gridId")
     @PutMapping("/elements/update/{elementId}")
     public void updateElement(@PathVariable("elementId") Integer elementId,  @RequestParam String name, String type, Double maximumBuyingPower, Double minimumBuyingPower, Double maximumSellingPower, Double minimumSellingPower) {
         eLementService.updateElement(elementId, name, type, maximumBuyingPower, minimumBuyingPower, maximumSellingPower, minimumSellingPower);
